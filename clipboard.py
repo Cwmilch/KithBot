3e12694aacb9cdefd65c2d74757e5eb07358f5bc
Cwmilch <milchcarter@yahoo.com>
2017-10-01 15:54:56 -0500

    Delete clipboard.py

--- clipboard.py
+++
@@ -1,7 +0,0 @@
-# Script used to quickly paste link to Dropbox file if needed, writes contents of clipboard to the file with the link
-from tkinter import Tk
-
-r = Tk()
-clip = r.selection_get(selection="CLIPBOARD")
-with open('C:/Users/carte/Dropbox/dropbox code.txt', 'w') as f:
-    f.write(clip)
